106b6ee01c444642cb56057f67b90fd96bea42b3
Thomas Cioppettini <tom@tecrails.com>
2019-02-21 10:48:53 -0500

    Initial commit

---
+++ lib/stripe_file_errors.ex
@@ -0,0 +1,18 @@
+defmodule StripeFileErrors do
+  @moduledoc """
+  Documentation for StripeFileErrors.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> StripeFileErrors.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/stripe_file_errors_test.exs
@@ -0,0 +1,8 @@
+defmodule StripeFileErrorsTest do
+  use ExUnit.Case
+  doctest StripeFileErrors
+
+  test "greets the world" do
+    assert StripeFileErrors.hello() == :world
+  end
+end
